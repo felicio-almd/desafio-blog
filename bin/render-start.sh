2431026ebc92776dab8f9d0e9ea3db7811e73e1e
Felicio <feliciorar@gmail.com>
2025-01-06 22:59:24 -0300

    fix

--- bin/render-start.sh
+++ bin/render-start.sh
@@ -7,4 +7,4 @@ bundle exec puma -t 5:5 -p ${PORT:-3000} -e ${RACK_ENV:-production} &
 sleep 10
 
 # Inicia o Sidekiq
-bundle exec sidekiq -L sidekiq.log -e production -C config/sidekiq.yml
+bundle exec sidekiq -e production -C config/sidekiq.yml
