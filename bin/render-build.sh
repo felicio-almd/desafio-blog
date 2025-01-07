fc1c4c316dd7f414d5d92c4e13e41b1dcaab7571
Felicio <feliciorar@gmail.com>
2025-01-06 21:46:33 -0300

    render build

---
+++ bin/render-build.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# bin/render-build.sh
+
+# Exit on error
+set -o errexit
+
+# Bundle install
+bundle install
+
+# Build Tailwind CSS
+bundle exec rails tailwindcss:build
+
+# Clean and precompile assets
+bundle exec rake assets:clean
+bundle exec rake assets:precompile
+
+# Run database migrations (não incluímos db:create porque o Render já cria o banco)
+bundle exec rake db:migrate
+
+# Rode as seeds apenas se necessário (descomente se precisar)
+# bundle exec rails db:seed
