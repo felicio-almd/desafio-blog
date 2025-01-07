996881e2bb47e091c04605844fecbf4bd24f114c
Felicio <feliciorar@gmail.com>
2025-01-06 21:31:52 -0300

    fix

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,13 @@
+redis_url = ENV.fetch('REDIS_URL')
+REDIS_CONFIG = {
+  url: redis_url,
+  ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+}
+
 Sidekiq.configure_server do |config|
-    config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/0') }
-  end
-  
-  Sidekiq.configure_client do |config|
-    config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/0') }
-  end
+  config.redis = REDIS_CONFIG
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = REDIS_CONFIG
+end
